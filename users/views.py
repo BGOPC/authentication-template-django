be6800bf11e3fba33623b0236cef9527a1b1467f
BGOPC <bb8933400@gmail.com>
2023-01-26 13:16:56 +0330

    temp update

--- users/views.py
+++ users/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
+
 
 # Create your views here.
 
 
-class UserView(View):
-    def get(self, request):
-        return render(request, 'users/user.html')
-    def post(self, request):
-        pass
+class UserView(TemplateView):
+    template_name = "users/user.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context edits later
+        return context
