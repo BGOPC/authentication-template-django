ce7ffe30b1635c8f190c74f8eaeb8d2b8c858fa4
BGOPC <bb8933400@gmail.com>
2023-01-27 21:50:37 +0330

    Final touches to login from (HopeFullyga *)

--- users/forms.py
+++ users/forms.py
@@ -25,4 +25,15 @@ class LoginForm(AuthenticationForm):
 
 
 class NewUserForm(UserCreationForm):
-    pass
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
