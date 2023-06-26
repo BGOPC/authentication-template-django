be6249a8acfec7fa4faa7024b4958292ad328538
BGOPC <bb8933400@gmail.com>
2023-06-26 13:16:04 +0330

    standardized user forms

--- users/forms.py
+++ users/forms.py
@@ -1,89 +1,91 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.utils.translation import gettext_lazy as _
 from phonenumber_field.formfields import PhoneNumberField
 from phonenumber_field.widgets import PhoneNumberPrefixWidget
+
 from users.models import User
 
 
 class LoginForm(AuthenticationForm):
-
-    def __init__(self, request=None, *args, **kwargs):
-        super().__init__(request=None, *args, **kwargs)
-        self.fields['username'].label = 'email'
-        self.fields['username'].widget = forms.TextInput(attrs={
-            "class": "appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 "
-                     "mb-3 leading-tight focus:outline-none focus:bg-white text-center",
-            "placeholder": "email"
+    username = forms.CharField(
+        label=_('Email'),
+        widget=forms.TextInput(attrs={
+            'class': 'appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 '
+                     'mb-3 leading-tight focus:outline-none focus:bg-white text-center',
+            'placeholder': _('Email')
         })
-        self.fields['password'].label = 'PassWord'
-        self.fields['password'].widget = forms.PasswordInput(attrs={'autocomplete': 'current-password',
-                                                                    "class": "appearance-none block w-full "
-                                                                             "bg-gray-200 text-gray-700 border "
-                                                                             "border-red-500 rounded py-3 px-4 mb-3 "
-                                                                             "leading-tight focus:outline-none "
-                                                                             "focus:bg-white text-center",
-                                                                    "placeholder": "Password"
-                                                                    })
+    )
+    password = forms.CharField(
+        label=_('Password'),
+        widget=forms.PasswordInput(attrs={
+            'autocomplete': 'current-password',
+            'class': 'appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 '
+                     'mb-3 leading-tight focus:outline-none focus:bg-white text-center',
+            'placeholder': _('Password')
+        })
+    )
 
 
 class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
-        "class": "appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 "
-                 "mb-3 leading-tight focus:outline-none text-center focus:bg-white",
-        "placeholder": "email"
-    }))
-    phone = PhoneNumberField(required=True, region="IR", widget=PhoneNumberPrefixWidget(
-        initial="IR",
-        attrs={
-            "class": "appearance-none block w-full bg-gray-200 text-center text-gray-700 border border-red-500 "
-                     "rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white",
-            "placeholder": "phone number"
-        }
-    ))
+    email = forms.EmailField(
+        label=_('Email'),
+        widget=forms.EmailInput(attrs={
+            'class': 'appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 '
+                     'mb-3 leading-tight focus:outline-none text-center focus:bg-white',
+            'placeholder': _('Email')
+        })
+    )
+    phone = PhoneNumberField(
+        label=_('Phone number'),
+        required=True,
+        region='IR',
+        widget=PhoneNumberPrefixWidget(
+            initial='IR',
+            attrs={
+                'class': 'appearance-none block w-full bg-gray-200 text-center text-gray-700 border border-red-500 '
+                         'rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white',
+                'placeholder': _('Phone number')
+            }
+        )
+    )
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['first_name'].label = 'First Name'
-        self.fields['first_name'].widget = forms.TextInput(attrs={
-            "class": "appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 "
-                     "mb-3 leading-tight focus:outline-none text-center focus:bg-white",
-            "placeholder": "FirstName"
+    first_name = forms.CharField(
+        label=_('First Name'),
+        widget=forms.TextInput(attrs={
+            'class': 'appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 '
+                     'mb-3 leading-tight focus:outline-none text-center focus:bg-white',
+            'placeholder': _('First Name')
         })
-        self.fields['last_name'].label = 'Last Name'
-        self.fields['last_name'].widget = forms.TextInput(attrs={
-            "class": "appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 "
-                     "mb-3 leading-tight focus:outline-none text-center focus:bg-white",
-            "placeholder": "LastName"
+    )
+    last_name = forms.CharField(
+        label=_('Last Name'),
+        widget=forms.TextInput(attrs={
+            'class': 'appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 '
+                     'mb-3 leading-tight focus:outline-none text-center focus:bg-white',
+            'placeholder': _('Last Name')
         })
-        self.fields['password1'].label = 'PassWord'
-        self.fields['password1'].widget = forms.PasswordInput(attrs={'autocomplete': 'current-password',
-                                                                     "class": "appearance-none block w-full "
-                                                                              "bg-gray-200 text-gray-700 border "
-                                                                              "border-red-500 rounded py-3 px-4 mb-3 "
-                                                                              "leading-tight focus:outline-none "
-                                                                              "focus:bg-white text-center",
-                                                                     "placeholder": "Password"
-                                                                     })
-        self.fields['password2'].label = 'RePeat PassWord'
-        self.fields['password2'].widget = forms.PasswordInput(attrs={'autocomplete': 'current-password',
-                                                                     "class": "appearance-none block w-full "
-                                                                              "bg-gray-200 text-gray-700 border "
-                                                                              "border-red-500 rounded py-3 px-4 mb-3 "
-                                                                              "leading-tight focus:outline-none "
-                                                                              "focus:bg-white text-center",
-                                                                     "placeholder": "Repeat Password"
-                                                                     })
+    )
+    password1 = forms.CharField(
+        label=_('Password'),
+        widget=forms.PasswordInput(attrs={
+            'autocomplete': 'current-password',
+            'class': 'appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 '
+                     'mb-3 leading-tight focus:outline-none focus:bg-white text-center',
+            'placeholder': _('Password')
+        }),
+        help_text=_("Your password should not be fully numeric and should have at least 8 characters.")
+    )
+    password2 = forms.CharField(
+        label=_('Repeat Password'),
+        widget=forms.PasswordInput(attrs={
+            'autocomplete': 'current-password',
+            'class': 'appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 '
+                     'mb-3 leading-tight focus:outline-none focus:bg-white text-center',
+            'placeholder': _('Repeat Password')
+        })
+    )
 
     class Meta:
         model = User
-        fields = ("first_name", "last_name", "email", "password1", "password2")
-        help_texts = {
-            'password1': """You password shouldn't be full numeric and should have at least 8 characters"""
-        }
-
-    def save(self, commit=True):
-        user = super(NewUserForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user
+        fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
